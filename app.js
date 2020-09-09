1af86373ee0a7213423c54ea7b5de95f79a224b0
kalu77 <kalujerovich77@hotmail.com>
2020-09-09 20:44:26 -0300

    update React

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-<h1>Hola</h1>
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.generarAleatorio = this.generarAleatorio.bind(this);
+    this.state = {
+      numero: 0
+    }
+  }
+  render() {
+    return (
+      <div>
+        <p>Número aleatorio: {this.state.numero}</p>
+        <button onClick={this.generarAleatorio}>Generar número aleatorio</button>
+      </div>
+    );
+  }
+   
+  generarAleatorio() {
+    const v=Math.trunc(Math.random()*10);
+    this.setState( {
+      numero: v
+    })
+  }
+}
+
+export default App;
